af6bb518b1e87f2606f9154217d00d6101401ee0
David Okaliwe <okaliwedavid@gmail.com>
2024-05-23 12:22:26 +0100

    Updates

--- app/page.js
+++ app/page.js
@@ -10,45 +10,37 @@ const Home = () => {
         <div className='justify-center w-full mx-auto
           max-w-[86rem] flex flex-col md:pt-12 ss:pt-20 pt-20' 
           >
-            <h1 className='text-main md:text-[80px] ss:text-[70px] 
+            <h1 className='text-black md:text-[80px] ss:text-[70px] 
             text-[52px] md:leading-[90px] fade-in-from-bottom
             ss:leading-[70px] leading-[55px] font-bold'>
-              The world's <span className="text-mainRed">
-              most exclusive </span> <br></br>
-              members-only <br className="ss:flex hidden"></br>club!
+              PROXY- <span className="text-primaryalt">
+              <br className="ss:flex hidden"></br>
+              Reinventing the </span> <br className="ss:flex hidden"></br>
+              real estate business.
             </h1>
 
             <p className='md:mt-6 ss:mt-5 mt-6 md:text-[18px] ss:text-[18px]
-            text-[16px] text-primary md:leading-[25px] ss:leading-[25px] 
-            leading-[20px] fade-in-from-bottom'
+            text-[16px] text-main md:leading-[25px] ss:leading-[25px] 
+            leading-[20px] fade-in-from-bottom md:max-w-[600px] 
+            ss:max-w-[480px] max-w-[320px]'
             style={{ opacity: 0, animationDelay: '0.5s', animationFillMode: 'forwards' }}>
-              We are a society of like-minded libertines with the highest 
-              standards of <br className="ss:flex hidden"></br>taste and style. We embrace unrestrained 
-              indulgence with <br className="ss:flex hidden"></br>one golden rule - consent.
+              Armed with two decades of industry wisdom, we are focused 
+              on combining these insights with the prowess of modern 
+              technology, delivering outcomes that not only meet but 
+              exceed your expectations.
             </p>
 
-            <div className='flex md:mt-8 ss:mt-8 mt-6 md:gap-8 ss:gap-5 
-            gap-3 items-center buttonfull fade-in-from-bottom2'
+            <div className='flex md:mt-6 ss:mt-8 mt-6 items-center 
+            buttonfull fade-in-from-bottom2'
             style={{ opacity: 0, animationDelay: '1s', animationFillMode: 'forwards' }}>
               <button
-                className='grow4 bg-secondary border-none buttonhalf
-                md:text-[17px] ss:text-[17px] text-[14px] md:py-4
-                ss:py-3 py-3 md:px-12 ss:px-7 px-3 text-primary 
+                className='grow4 bg-primaryalt border-none buttonhalf
+                md:text-[18px] ss:text-[17px] text-[14px] md:py-4
+                ss:py-3 py-3 md:px-24 ss:px-7 px-3 text-white 
                 md:rounded-[6px] ss:rounded-[3px] rounded-[3px] 
-                font-medium font-manier cursor-pointer'
+                font-medium cursor-pointer'
               >
-                Become a Member
-              </button>
-
-              <button
-                className='border-[1px] grow2 border-secondary 
-                md:text-[17px] ss:text-[17px] text-[14px] md:py-4 
-                ss:py-3 py-3 md:px-20 ss:px-14 px-6 text-secondary 
-                md:rounded-[6px] ss:rounded-[3px] rounded-[3px] 
-                font-medium bg-transparent font-manier cursor-pointer
-                buttonhalf'
-              >
-                Contact Us
+                Join Us
               </button>
             </div>
         </div>

3f5f98dbae4403f606fe50c2548b1c02584d53ab
David Okaliwe <okaliwedavid@gmail.com>
2024-06-05 10:37:29 +0100

    Updates

--- constants/ContactButton.jsx
+++ constants/ContactButton.jsx
@@ -2,8 +2,8 @@
 
 const ContactButton = () => {
   return (
-    <div className={`bg-main py-3 px-6 cursor-pointer rounded-l-md
-    bg-opacity-20 bottom-28 right-0 z-20 fixed`}>
+    <div className='bg-main py-3 px-6 cursor-pointer rounded-l-md
+    bottom-28 right-0 z-50 fixed grow2'>
       <p className="text-white text-[20px] font-semibold">
         Contact Us
       </p>
